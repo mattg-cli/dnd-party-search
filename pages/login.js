9afaba54b0d1d87021eeded78c0f0bda52e80434
Matt Gilbert <mattgilbert042@gmail.com>
2021-01-30 18:23:10 -0600

    login mostly complete, i want to mess with it more to see if theres something else that i like

--- pages/login.js
+++ pages/login.js
@@ -7,34 +7,38 @@ export default function Login() {
       <section className={`vh-100 ${styles.background}`}>
         <div className="container-fluid vh-100">
           <div className="row">
-            <div className="col-12 col-lg-5 bg-opaque vh-100">
+            <div className="col-12 col-lg-6 bg-light shadow-lg vh-100">
+
               <div className="row h-100 justify-content-center align-items-center">
-                <div className="col-10 col-md-8 col-lg-6 py-3 bg-light">
-                  <h4 className="display-4 text-center">LOGIN</h4>
-                  <form className="px-4 py-3">
-                  <div className="form-group">
-                    <label for="email">Email address</label>
-                    <input type="email" className="form-control" id="email" placeholder="email@example.com" />
-                  </div>
-                  <div className="form-group">
-                    <label for="pwd">Password</label>
-                    <input type="password" className="form-control" id="pwd" placeholder="Password" />
-                  </div>
-                  <div className="form-check">
-                    <input type="checkbox" className="form-check-input" id="dropdownCheck" />
-                    <label className="form-check-label" for="dropdownCheck">
-                      Remember me
-                    </label>
-                  </div>
-                  <div className="text-center pt-3">
-                    <button type="submit" className="btn btn-default">Login</button>
-                  </div>
-                </form>
-                {/* <div className="dropdown-divider"></div> */}
-                {/* <a className="dropdown-item" href="#">New around here? Sign up</a>
-                <a className="dropdown-item" href="#">Forgot password?</a> */}
+                <div className="col-12 col-md-8 col-lg-6 py-3 text-default ">
+                  <h4 className="display-4 text-center mb-4">LOGIN</h4>
+                  <form className="">
+                    <div className="form-group">
+                      <label htmlFor="email">USER NAME or EMAIL ADDRESS</label>
+                      <input type="email" className="form-control" id="email" placeholder="email@example.com" />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="pwd">PASSWORD</label>
+                      <input type="password" className="form-control" id="pwd" placeholder="" />
+                    </div>
+                    <div className="form-check">
+                      <input type="checkbox" className="form-check-input" id="dropdownCheck" />
+                      <label className="form-check-label" htmlFor="dropdownCheck">
+                        REMEMBER ME
+                      </label>
+                    </div>
+                    <div className="text-center my-3">
+                      <button type="submit" className="btn btn-default w-100">LOGIN</button>
+                    </div>
+                  </form>
+                
+                <a href="#">New around here? Sign up</a>
+                <a className="float-right" href="#">Forgot password?</a>
+                <div className="separator">OR</div>
+                <button className="btn btn-secondary mb-2 py-4 w-100">test</button>
                 </div>
               </div>
+              
             </div>
           </div>
         </div>
